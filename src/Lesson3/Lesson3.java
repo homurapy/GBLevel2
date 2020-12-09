c68ddc114488180e01f8ceabc10085e59eec0e3a
Alex <2640178@gmail.com>
2020-12-09 10:18:34 +0300

    Задание 5. Уровень 2

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -4,9 +4,24 @@ import java.util.*;
 
 public class Lesson3 {
     public static void main (String[] args) {
-        String[] array = {"map", "temp", "make", "folk", "dot", "food", "take", "put", "get", "pop", "send", "reset", "dot", "folk", "folk", "get","map"};
+        String[] array = {"map", "temp", "make", "folk", "dot", "dot", "food", "take", "put", "get", "pop", "send", "reset", "dot", "folk", "folk", "get","map"};
         ArrayList < String > arrayList = new ArrayList<>(Arrays.asList(array));
-        Set wordUnique = new HashSet();
+        Set wordUnique = new HashSet(Arrays.asList(array));
+        Iterator<String> iter = wordUnique.iterator();
+
+        while (iter.hasNext()){
+            String s = iter.next();
+            int countWord = 0;
+            for( int j = 0; j< arrayList.size();j++){
+                if(s.equals(arrayList.get(j))){
+                    countWord += 1;
+                }
+            }
+            System.out.println(s + " " + countWord);
+
+        }
+
+        Set wordUn = new HashSet();
 
         for (int i=0; i< arrayList.size(); i++) {
             int countWord = 0;
@@ -16,8 +31,8 @@ public class Lesson3 {
                     countWord += 1;
                 }
             }
-            wordUnique.add(wordFind+" "+countWord);
+            wordUn.add(wordFind+" "+countWord);
         }
-            System.out.println(wordUnique);
-        }
+        System.out.println(wordUn);
+    }
 }
